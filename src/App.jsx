474ee5f03601058521d1129f321dc6b96e17d35f
ramirokhouri <ramirokhouri@hotmail.com>
2023-10-09 20:46:06 -0300

    fetch de los datos de https://jsonplaceholder.typicode.com/users en el componente App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
   return (
     <div className="App">
       <h1>Fetch Like a PRO</h1>
-      <div className="card"></div>
+      <div className="card">
+        <ul>
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
