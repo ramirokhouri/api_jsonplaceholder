368d9724187f5cf1462c1770eebc56a745d1048a
ramirokhouri <ramirokhouri@hotmail.com>
2023-10-11 00:03:25 -0300

    loading state for loading content

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useFetch } from "./useFetch";
 import "./App.css";
 
 function App() {
-  const { data } = useFetch("https://jsonplaceholder.typicode.com/users");
+  const { data, loading } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
   return (
     <div className="App">
       <h1>Fetch Like a PRO</h1>
       <div className="card">
         <ul>
+          {loading && <li>Loading...</li>}
           {data?.map((user) => (
             <li key={user.id}>{user.name}</li>
           ))}
